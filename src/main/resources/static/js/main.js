0f74fd1f3004c3c029286f09312151c199beb6d2
tony9701 <leesin9701@gmail.com>
2024-11-02 11:07:28 +0200

    added sing up form
    added sign up form and made the both display depends on what you need.
    e.g.  if you need the "sign in" form the "sign up" is not displayed using JS

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,9 @@
-document.querySelectorAll('.input-box input').forEach(input => {
-    input.addEventListener('input', () => {
-        if (input.value) {
-            input.classList.add('has-content');
-        } else {
-            input.classList.remove('has-content');
-        }
-    });
-});
+function showRegister() {
+    document.querySelector('.form-box.login').style.display = 'none';
+    document.querySelector('.form-box.register').style.display = 'flex';
+}
+
+function showLogin() {
+    document.querySelector('.form-box.register').style.display = 'none';
+    document.querySelector('.form-box.login').style.display = 'flex';
+}
