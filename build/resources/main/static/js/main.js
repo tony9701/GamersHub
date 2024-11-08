0214740b6f04833c6dcca7d59798bd3450998eb0
tony9701 <leesin9701@gmail.com>
2024-11-08 19:10:10 +0200

    back-end config
    application.yaml, jdbc driver  and others

---
+++ build/resources/main/static/js/main.js
@@ -0,0 +1,9 @@
+function showRegister() {
+    document.querySelector('.form-box.login').style.display = 'none';
+    document.querySelector('.form-box.register').style.display = 'flex';
+}
+
+function showLogin() {
+    document.querySelector('.form-box.register').style.display = 'none';
+    document.querySelector('.form-box.login').style.display = 'flex';
+}
