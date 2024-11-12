1943d81d382e472aa4d5155357fdc0e89418d061
tony9701 <leesin9701@gmail.com>
2024-11-12 21:11:30 +0200

    user entity created!

---
+++ src/main/java/com/GamersHub/model/entity/User.java
@@ -0,0 +1,34 @@
+package com.GamersHub.model.entity;
+
+import com.GamersHub.model.entity.baseEntity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    @Size(min = 6, max = 20)
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false)
+    private int age;
+}
