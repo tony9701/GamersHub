7a43019c29e9319da5b3399cefdb487c5e2b099a
tony9701 <leesin9701@gmail.com>
2024-11-13 20:17:05 +0200

    First HTTP Request Created

--- src/main/java/com/GamersHub/model/web/HomeController.java
+++ src/main/java/com/GamersHub/model/web/HomeController.java
@@ -1,7 +1,13 @@
 package com.GamersHub.model.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 }
