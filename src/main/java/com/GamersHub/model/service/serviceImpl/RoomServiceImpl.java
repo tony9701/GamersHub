807a28fc780f95ce28cbb6df82031677b02666e8
tony9701 <leesin9701@gmail.com>
2024-11-13 20:13:13 +0200

    Created some services

---
+++ src/main/java/com/GamersHub/model/service/serviceImpl/RoomServiceImpl.java
@@ -0,0 +1,8 @@
+package com.GamersHub.model.service.serviceImpl;
+
+import com.GamersHub.model.service.RoomService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomServiceImpl implements RoomService {
+}
---
+++ src/main/java/com/GamersHub/model/service/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.GamersHub.model.service.serviceImpl;
+
+import com.GamersHub.model.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
