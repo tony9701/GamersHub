fae3dd91cbfda2ccd5522ca5bd6fa86f454e0c76
tony9701 <leesin9701@gmail.com>
2024-11-13 20:00:25 +0200

    Created GameRepository

---
+++ src/main/java/com/GamersHub/model/repository/GameRepository.java
@@ -0,0 +1,9 @@
+package com.GamersHub.model.repository;
+
+import com.GamersHub.model.entity.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
