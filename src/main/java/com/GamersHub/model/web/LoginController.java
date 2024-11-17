e1b5775c2a3a93a1236228449675c2abb7886d58
tony9701 <leesin9701@gmail.com>
2024-11-17 21:05:41 +0200

    sing in view created

--- src/main/java/com/GamersHub/model/web/LoginController.java
+++ src/main/java/com/GamersHub/model/web/LoginController.java
@@ -1,7 +1,13 @@
 package com.GamersHub.model.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "sign-in-up";
+    }
 }
