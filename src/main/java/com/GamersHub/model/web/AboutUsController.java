06418f9f7f1ac4caefafc9012a9da1574fab6393
tony9701 <leesin9701@gmail.com>
2024-11-17 21:02:22 +0200

    about-us view added

--- src/main/java/com/GamersHub/model/web/AboutUsController.java
+++ src/main/java/com/GamersHub/model/web/AboutUsController.java
@@ -1,4 +1,14 @@
 package com.GamersHub.model.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AboutUsController {
+
+    @GetMapping("/about-us")
+    public String aboutUsView(Model model) {
+        return "about-us";
+    }
 }
