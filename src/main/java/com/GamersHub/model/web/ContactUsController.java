25cb642bcb35a8f72b2fdf580d2b292505c68971
tony9701 <leesin9701@gmail.com>
2024-11-17 21:00:48 +0200

    contact-us view added

--- src/main/java/com/GamersHub/model/web/ContactUsController.java
+++ src/main/java/com/GamersHub/model/web/ContactUsController.java
@@ -1,4 +1,13 @@
 package com.GamersHub.model.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ContactUsController {
+
+    @GetMapping("/contact-us")
+    public String contactUsView() {
+        return "contact-us";
+    }
 }
