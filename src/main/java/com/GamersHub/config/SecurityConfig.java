ce18f4d4feeedaeb504d6ecb530ab11afbcae9c6
tony9701 <leesin9701@gmail.com>
2024-11-19 20:43:45 +0200

    spring security password encoder added

---
+++ src/main/java/com/GamersHub/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.GamersHub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    //TODO add when implement Spring Security!
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+}
