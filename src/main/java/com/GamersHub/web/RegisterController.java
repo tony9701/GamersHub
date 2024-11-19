eca04fd5f43ef41daf355e77a068d8e3fd16b1ca
tony9701 <leesin9701@gmail.com>
2024-11-19 20:30:59 +0200

    registration end point test
    tested if the form send correct data to the dto

--- src/main/java/com/GamersHub/web/RegisterController.java
+++ src/main/java/com/GamersHub/web/RegisterController.java
@@ -1,8 +1,11 @@
 package com.GamersHub.web;
 
+import com.GamersHub.model.dto.registerUserDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class RegisterController {
@@ -14,7 +17,7 @@ public class RegisterController {
     }
 
     @PostMapping("/register")
-    public String register() {
+    public String register(@Valid registerUserDTO registerUserDTO, RedirectAttributes redirectAttributes) {
         System.out.print("TEST");
         return "sign-in-up";
     }
