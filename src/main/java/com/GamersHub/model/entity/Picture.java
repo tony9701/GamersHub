7d11ecd7e0c178f6b22bd96270087cc7ba75bb3f
tony9701 <leesin9701@gmail.com>
2024-11-25 20:26:36 +0200

    created picture entity

---
+++ src/main/java/com/GamersHub/model/entity/Picture.java
@@ -0,0 +1,15 @@
+package com.GamersHub.model.entity;
+
+import com.GamersHub.model.entity.baseEntity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Picture extends BaseEntity {
+
+}
