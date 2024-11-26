f31157c575a153d309c6828b9635e9515b923515
tony9701 <leesin9701@gmail.com>
2024-11-26 20:18:47 +0200

    home controller upd
    if user is logged in will display home page if user is not logged in will display login/register page

--- src/main/java/com/GamersHub/web/HomeController.java
+++ src/main/java/com/GamersHub/web/HomeController.java
@@ -6,13 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(@AuthenticationPrincipal UserDetails userDetails, Model model) {
-        if (userDetails != null) {
-            model.addAttribute("userDetails", userDetails);
+    public String home(Principal principal, Model model) {
+        if (principal != null) { // if person is logged in
+//            model.addAttribute("userDetails", principal);
             return "index";
         }
 
