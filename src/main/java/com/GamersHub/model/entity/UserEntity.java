bb825e640199a5192d4305754d414fb27955bf67
tony9701 <leesin9701@gmail.com>
2024-11-27 16:53:52 +0200

    added some relationships with other entities

--- src/main/java/com/GamersHub/model/entity/UserEntity.java
+++ src/main/java/com/GamersHub/model/entity/UserEntity.java
@@ -1,13 +1,13 @@
 package com.GamersHub.model.entity;
 
 import com.GamersHub.model.entity.baseEntity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -30,4 +30,27 @@ public class UserEntity extends BaseEntity {
 
     @Column()
     private int age;
+
+    @ManyToMany(
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<UserRole> roles;
+
+    @ManyToMany(
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "users_games",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "game_id")
+    )
+    private List<Game> games;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private ProfilePicture profilePicture;
 }
